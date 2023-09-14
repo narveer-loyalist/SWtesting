943672853a1f4d7f32d48c90f9e33848f86e72e5
Narveer Saharan <134150520+narveer-loyalist@users.noreply.github.com>
2023-09-14 19:08:19 -0400

    Variable Declarations

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	var num int = 2
+	fmt.Println(num)
+	echo()
+}
+
+func echo() {
+	var class_name string = "SW Testing"
+	class_name = "SW Testing by Maziar\n"
+	class_num := 214
+	msg := "\nGo lang Rocks!!"
+	fmt.Println(class_name, class_num, msg)
 }
